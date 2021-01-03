1604b23d26c6081f0bc3746c60f5f04e68444874
JaehyunYoo <nike361@naver.com>
2021-01-03 22:44:00 +0900

    Add:Login 페이지 필수 구현사항 완료

---
+++ src/Pages/yoojaehyun/Main/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+          <h1>메인입니다.</h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
